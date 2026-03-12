6179c6d73e6c1192966b85fa9b3d21dcd140a5bb
hasininaluvala2509-arch <hasininaluvala2509@gmail.com>
2026-03-12 19:44:40 +0530

    Add files via upload

---
+++ week02/3.js
@@ -0,0 +1,136 @@
+let person ={
+    name :'ravi',
+    age: 19
+}
+
+//adding new property 
+person.city='hyd'
+//console.log(person)
+
+//update a property  obj is not index based so no insertion at end or start or middle wt ever
+person.name='bhanu'
+//console.log(person)
+
+//delete a property
+delete person.name
+//console.log(person)
+
+let testData = [90,45,-12,65,73]
+console.log(testData)
+
+//filter(selection)   taking only required things only
+    // ele > 30
+/*          without filter method
+let result =[]
+for(let ele of testData){
+    if(ele>30)
+        result.push(ele)
+}
+console.log(result)*/
+
+let r=testData.filter(function(element){
+    return element>30
+})
+//console.log(r)
+
+//arrow function
+
+let r1= testData.filter((element) => element>30)
+//console.log(r1)
+
+//get ele between 40 and 80
+let r2= testData.filter(element=> element>=40 && element<=80)
+//console.log(r2)
+
+let r3=testData.filter(element=>element+10)
+//console.log(r3)// no change because its work is to select not to modify
+
+//map (modify)
+//add 10 for each ele
+let r4=testData.map(ele=>ele+10)
+//console.log(r4)
+
+//add 10 for ele <50 and sub 20 from >50
+let r5=testData.map(function(ele){
+    if(ele<50)
+        return ele+10;
+    else 
+        return ele-20;
+})
+//console.log(r5) 
+
+let r6=testData.map(ele=>ele>10)
+//console.log(r6) //for selection it returns boolean values
+
+//reduce   array to single value
+// find sum of testData
+const sum=testData.reduce((accumulator,element)=> accumulator+element)   //pakka 2 parameters required
+//accumulator stores the value of previours operation  
+// for the first time (iteration) accu stores array[0] and ele stores array[1]
+//console.log(sum)
+
+//find smallest nd big ele
+const big=testData.reduce((a,b)=> (a>b)?a:b)
+//console.log(big)
+
+const small=testData.reduce((a,b)=> (a<b)?a:b)
+//console.log(small)
+
+//find(ele) findIndex(with index)
+//search 25
+let ans=testData.find(ele=>ele===25)
+//console.log(ans)
+let ans1=testData.findIndex(ele=>ele===-12)
+//console.log(ans1)
+
+//sort
+let data=[9,10,8,4]
+let ascending=data.toSorted((a,b)=>a-b) //to ensure +ve or -ve nd 0 it compares a nd b to sort
+//console.log(data)
+//console.log("new ascending array is",ascending)
+//console.log(data)
+let descending=data.sort((a,b)=>b-a)
+//console.log("new descending array is",descending)
+//makes lecsical(bitwise) (string) level comparision 
+//console.log(data)
+
+const student=[
+    {id:1,name:'Ravi',marks:78},
+    {id:2,name:'Bhau',marks:58},
+    {id:3,name:'Venna',marks:98},
+    {id:4,name:'Vasu',marks:88},
+    {id:5,name:'Raju',marks:48},
+]
+
+let res=student.filter(studentobj=>studentobj.marks>80)
+//console.log(res)
+
+//find sum of marks of all students
+let sumOfMarks=student.reduce((acc,stdObj)=>acc+stdObj.marks,0)
+//console.log(sumOfMarks)//returns NaN i.e not a number without  ,0 nd =>acc.marks instead of acc 
+//so we set intial val to acc i.e (,0)  so acc recives 0 as intial val
+
+let data1=student.map(stuObj=>stuObj)
+//console.log("............",data1)//to see all values
+
+//what is error
+//console.log(a)//a is not defined
+//console.log([1,2,3].filter())
+
+//creating new error
+//const err=new Error("ur abnormal !!!!")
+//console.log(err.name)
+//console.log(err.message)
+//console.log(err.stack)
+
+
+//Handling error
+console.log("first")
+try{
+console.log(a)//a is not define
+}
+catch(err){
+    console.log(err.message)
+}//error handled cause our application has catch block
+console.log("Second")//not executed due to error in previous line 129
+console.log("third")
---
+++ week02/4.js
@@ -0,0 +1,250 @@
+//object literal (obj vth out class in js)
+/*const test ={
+    a:10,
+    getData:function(){}
+}
+
+class Student{
+    //properties  //no need of let nd undefined by default
+    #sno;  //private variable
+    name;
+    email;
+
+    //constructor
+    constructor(sno,name,email){
+        //initialze obj
+        this.#sno=sno;
+        this.name=name;
+        this.email=email;
+    }
+
+    //method
+    getStudentName(){
+        return this.name
+    }
+
+}
+let std1=new Student(24,"Mani","manoharpatel.gmail.com") //constructor call
+let std2=new Student(24,"Ravi","ravi.gmail.com")
+console.log(std1.sno)//undefined
+
+class Employee{
+    #eno
+    #ename
+    constructor(eno,ename){
+        this.#eno=eno
+        this.#ename=ename
+    }
+    //instance method
+    getData(){
+        console.log("Employee name is : ",this.#ename,"and Id : ",this.#eno)
+    }
+    //static method
+    static testMethod(){
+
+        return this.test;
+    }
+
+}
+
+let emp1=new Employee(101,'Ravi')
+emp1.getData()
+
+Employee.testMethod();
+//obj literal 
+let emp2={
+    eno:201,
+    ename:'nam',
+    eadd:{
+        city:'hyd',
+        pin: 90
+    }
+}
+
+//optional changing ? & nullish c   ??
+const person={
+    pid:100,
+    name:'ravi'
+}
+console.log(person.pid)
+console.log(person.name)
+console.log(person.Marks)//undefined
+//console.log(person.Marks.length)//TypeError: 
+console.log(person.Marks?.length)//go to person obj and check if there is Marks in it
+console.log(person.Marks?.length??"Marks not defined")//go to person obj and check if there is Marks in it
+console.log(person.address?.city?.length)
+
+
+//spread operator (create copies of arrays 7 objects )
+let x=100
+//create copy
+let y=x//in mem y=100,x=100   x changes y does not get effected
+//but in obj both are refering to object in heap mem  change reflects in both object's-> original and duplicated
+
+let obj={a: 100}
+let cpyObj=obj
+obj.a=123
+console.log(obj)
+console.log(cpyObj)
+
+//creating copy of object (using spread operator)  shallow copy
+
+let originalObj={a: 10,b:20}
+let copyObj={...originalObj}
+originalObj.a=123
+console.log(originalObj)
+console.log(copyObj)
+
+let originalArr=[10,2,3]
+let copyArr=[...originalArr]
+originalArr.pop()
+console.log(originalArr)
+
+//deep copy
+let person1={name:'ravi',address:{city:"hyd"}}
+//let cpyPerson1=structuredClone(person1)  or
+let cpyPerson1=structuredClone(person1)
+person1.address.city='wrngl'
+person1.name='hasi'
+console.log(person1)
+console.log(cpyPerson1)
+
+//add elements/prop while copying
+let arr=[1,2,3]
+let cpyarr=[...arr,4]
+console.log(arr)
+console.log(cpyarr)
+
+let o={a:10}
+let cpyo={...o,b:20}
+
+//merge
+let a1=[10,20]
+let a2=[30,40]
+let mergeda=[...a1,...a2]
+console.log(mergeda)
+
+//Rest parameter
+function test(a,b){
+    console.log(a,b)
+}
+
+test(100)//100 undefined
+test(10,20,90)//10 20
+
+function test1(...a){//considers n number of ele and binds in array
+    console.log(a)
+}
+
+test1(100)//100 
+test1(10,20,90)//10 20 30
+
+function test2(...a,b){//SyntaxError: Rest parameter must be last formal parameter
+    console.log(a,b)
+}
+
+test1(100)
+test1(10,20,90)
+function test2(a,...b){//SyntaxError: Rest parameter must be last formal parameter
+    console.log(a,b)
+}
+
+test1(100)//100
+test1(10,20,90)//10 [20,30]
+
+function sumOf(...a){
+    console.log(a)
+    console.log("sum of elements is : ",a.reduce((a,b)=>a+b))
+}
+sumOf(10,10,10,10,10,10)
+
+//destructuring(unpacking)
+
+let arr=[10,20,30]
+let [a,b,c]=arr//a=10,b=20,c=30
+
+let emp={
+    name:'hasi',
+    sal:6000000,
+    address:{
+        city:'hyd',
+        pin:909090
+    }
+}
+let {name,sal,address:{city}}=emp
+console.log(name,emp.name)
+
+console.log("first")
+for(let i=0;i<10000000000;i++){}
+console.log("Second")
+console.log("3rd")
+
+console.log("first")
+setTimeout(()=>{
+    console.log("Task completed")
+},5000)
+console.log("Second")
+console.log("3rd")
+
+
+//timer function
+// setTimeout(()=>{
+//     console.log("After 3 seconds")
+// },3000)
+
+console.log("he'll")
+setInterval(()=>{
+    console.log("timer called")
+},2000)//won't stop untl ctrl+c
+
+
+//promise
+// i'll call u in 5 min
+//creation(creator/produce) consuming(consumer)
+
+//creation of promise
+console.log("Friend is waiting a call in 5 Seconds")
+let futureCondition=true
+
+const promiseObj=new Promise((fulfilled,rejected)=>{
+    setTimeout(()=>{
+        if(futureCondition===true)
+    fulfilled("Promise fulfilled")
+        else
+    rejected("Promise rejected")
+    },5000)
+})
+    console.log(promiseObj)
+promiseObj
+.then((msg)=>console.log(msg))
+.catch((err)=>console.log("Error : ",msg))
+j
+
+//i'll send 10k by tommorow
+
+console.log("Send 10k to friend")
+let cashAvailable=false
+const prom = new Promise((a,b)=>{
+    setTimeout(()=>{
+        if(cashAvailable)
+            a("Payment Successful")
+        else
+            b("Insufficient bank balance")
+    },1000)
+})
+console.log(prom)
+
+prom.then((msg)=>console.log(msg)).catch((msg)=>console.log(msg))
+*/
+
+//Examples of prom 
+
+//Make api req
+fetch('https://jsonplaceholder.typicode.com/posts')//time req is unpredictable
+.then(res=>res.json())
+.then(res=>console.log("res is ",res))
+.catch(err=>console.log("Error : ",err))
+//hash a pass
+//creating tokens
+//database / HTTP libraries
+//files & stream APIs
